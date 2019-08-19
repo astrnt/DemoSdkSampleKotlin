06753753bb1c7a8030b806b49839abb0c9ea31a7
Deni Rohimat <rohimat@astrnt.co>
2019-08-19 13:11:18 +0700

    change compress video library

--- app/src/main/java/co/astrnt/kyck/features/sendingvideo/SendingVideoActivity.kt
+++ app/src/main/java/co/astrnt/kyck/features/sendingvideo/SendingVideoActivity.kt
@@ -9,22 +9,18 @@ import co.astrnt.kyck.features.base.BaseActivity
 import co.astrnt.kyck.features.success.SuccessActivity
 import co.astrnt.kyck.util.DialogFactory
 import com.orhanobut.hawk.Hawk
+import com.otaliastudios.transcoder.Transcoder
+import com.otaliastudios.transcoder.TranscoderListener
 import kotlinx.android.synthetic.main.activity_sending_file.*
 import kotlinx.android.synthetic.main.toolbar.*
 import net.gotev.uploadservice.*
-import net.ypresto.androidtranscoder.MediaTranscoder
-import net.ypresto.androidtranscoder.strategy.DefaultAudioStrategy
-import net.ypresto.androidtranscoder.strategy.DefaultAudioStrategy.AUDIO_CHANNELS_AS_IS
-import net.ypresto.androidtranscoder.strategy.DefaultVideoStrategies
 import java.io.File
-import java.util.concurrent.Future
 
 class SendingVideoActivity : BaseActivity(), UploadStatusDelegate {
 
     private lateinit var outputPath: String
     private lateinit var candidateId: String
     private lateinit var srcPath: String
-    private var future: Future<Void>? = null
 
     companion object {
 
@@ -43,8 +39,8 @@ class SendingVideoActivity : BaseActivity(), UploadStatusDelegate {
         initToolbar(toolbar, "Step 2")
         txtMessage.text = "Sending your video"
 
-        candidateId = Hawk.get<String>("CandidateId")
-        srcPath = Hawk.get<String>("VideoFilePath")
+        candidateId = Hawk.get("CandidateId")
+        srcPath = Hawk.get("VideoFilePath")
 
         doCompressVideo()
     }
@@ -58,17 +54,11 @@ class SendingVideoActivity : BaseActivity(), UploadStatusDelegate {
 
         outputPath = File(directory, "output_" + System.currentTimeMillis() + "_video.mp4").absolutePath
 
-        future = MediaTranscoder.into(outputPath)
-                .setDataSource(srcPath)
-                .setVideoOutputStrategy(DefaultVideoStrategies.for360x480())
-                .setAudioOutputStrategy(DefaultAudioStrategy(AUDIO_CHANNELS_AS_IS))
-                .setListener(object : MediaTranscoder.Listener {
+        Transcoder.into(outputPath)
+                .addDataSource(srcPath)
+                .setListener(object : TranscoderListener {
                     override fun onTranscodeCompleted(successCode: Int) {
                         doUploadVideo()
-                    }
-
-                    override fun onTranscodeFailed(exception: Throwable) {
-                        DialogFactory.createErrorDialog(context, "Video Compress Transcoder error occurred").show()
                     }
 
                     override fun onTranscodeProgress(progress: Double) {
@@ -77,6 +67,11 @@ class SendingVideoActivity : BaseActivity(), UploadStatusDelegate {
                     override fun onTranscodeCanceled() {
                         DialogFactory.createErrorDialog(context, "Video Compress Transcoder canceled").show()
                     }
+
+                    override fun onTranscodeFailed(exception: Throwable) {
+                        DialogFactory.createErrorDialog(context, "Video Compress Transcoder error occurred").show()
+                    }
+
                 }).transcode()
     }
 
